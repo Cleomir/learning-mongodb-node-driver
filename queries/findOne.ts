a12b4975d0a1402eb53bea46d93d16840c024017
Cleomir <cleomir.mg@hotmail.com>
2020-09-28 14:02:25 +0300

    Rename and refactor file

--- queries/findOne.ts
+++ queries/findOne.ts
@@ -1,21 +1,22 @@
 import { MongoClient, Db, Collection } from "mongodb";
 
-const uri = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const uri =
+  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
 
-const client = new MongoClient(uri, {useUnifiedTopology: true});
-
-(async function run(){
+(async function run() {
   try {
     await client.connect();
 
     const database: Db = client.db("sample_mflix");
     const collection: Collection<any> = database.collection("movies");
-    const query = { title: "Back to the future" };
-    const movie = await collection.findOne(query);
+    const movie: any = await collection.findOne({
+      title: "Back to the future",
+    });
 
     console.log(movie);
     await client.close();
   } catch (error) {
     console.error("Error: ", error);
-  } 
+  }
 })();
