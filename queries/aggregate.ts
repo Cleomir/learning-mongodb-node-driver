45feb3ddf2518cccdd02daf1cdeb1e2668d46d0c
Cleomir <cleomir.mg@hotmail.com>
2020-09-29 09:32:36 +0300

    Add aggregation

---
+++ queries/aggregate.ts
@@ -0,0 +1,31 @@
+import { MongoClient, Db, Collection, AggregationCursor } from "mongodb";
+
+const uri =
+  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+(async function run() {
+  try {
+    await client.connect();
+
+    const database: Db = client.db("db_name");
+    const collection: Collection<any> = database.collection("collections_name");
+    const aggregationResult: AggregationCursor = collection.aggregate([
+      {
+        $match: {
+          date: {
+            $gte: new Date(new Date().getTime() - 1000 * 3600 * 24 * 7),
+            $lt: new Date(),
+          },
+        },
+      },
+    ]);
+
+    (await aggregationResult.toArray()).forEach((element) =>
+      console.log(element)
+    );
+    await client.close();
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+})();
