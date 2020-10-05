e0c965ddb54e12923bd390e929dcc9e4b4564f02
Cleomir <cleomir.mg@hotmail.com>
2020-10-05 09:13:58 +0300

    Add unwind

---
+++ aggregation/unwind.ts
@@ -0,0 +1,30 @@
+import { MongoClient, Db, Collection, AggregationCursor } from "mongodb";
+
+const uri =
+  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+(async function run() {
+  try {
+    await client.connect();
+
+    const database: Db = client.db("db_name");
+    const collection: Collection<any> = database.collection("collections_name");
+    const aggregationResult: AggregationCursor = collection.aggregate([
+      {
+        $unwind: {
+          path: "$property",
+          includeArrayIndex: "optional_name_for_index",
+          preserveNullAndEmptyArrays: false,
+        },
+      },
+    ]);
+
+    (await aggregationResult.toArray()).forEach((element) =>
+      console.log(element)
+    );
+    await client.close();
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+})();
