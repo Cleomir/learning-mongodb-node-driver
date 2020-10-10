a6ecf3d12c1d097e59694f94ce7d909a47588a2c
Cleomir <cleomir.mg@hotmail.com>
2020-10-10 11:34:55 +0300

    Add updateAllArrayItems

---
+++ update/updateAllArrayItems.ts
@@ -0,0 +1,30 @@
+import { MongoClient, Db, Collection, UpdateWriteOpResult } from "mongodb";
+
+const uri =
+  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+(async function run() {
+  try {
+    await client.connect();
+
+    const database: Db = client.db("db_name");
+    const collection: Collection<any> = database.collection("collections_name");
+    const document = [
+      {
+        $set: {
+          "array.$[].anotherProperty": "test value",
+        },
+      },
+    ];
+    const result: UpdateWriteOpResult = await collection.updateOne(
+      { property: "value" },
+      document
+    );
+
+    console.log(result.result);
+    await client.close();
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+})();
