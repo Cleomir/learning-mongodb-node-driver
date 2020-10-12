c24f10256298c2528335cce8e09a2efac4ffb7e6
Cleomir <cleomir.mg@hotmail.com>
2020-10-12 09:24:33 +0300

    Add comparison operators

---
+++ operators/comparison/index.ts
@@ -0,0 +1,16 @@
+// $eq(equal)
+const eq = { property: { $eq: "value" } };
+// $ne(not equal)
+const ne = { property: { $ne: "value" } };
+// $gt(greater than)
+const gt = { property: { $gt: "value" } };
+// $gte(greater than or equal)
+const gte = { property: { $gte: "value" } };
+// $lt(less than)
+const lt = { property: { $lt: "value" } };
+// $lte(less than or equal)
+const lte = { property: { $lte: "value" } };
+// $in
+const $in = { property: { $in: ["value", "value"] } };
+// $nin(not in)
+const nin = { property: { $nin: ["value", "value"] } };
