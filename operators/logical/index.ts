384f1f3055f4df351e41946e0846fd9a507bf7a7
Cleomir <cleomir.mg@hotmail.com>
2020-10-13 10:44:14 +0300

    Add logical operators

---
+++ operators/logical/index.ts
@@ -0,0 +1,8 @@
+// $and
+const and = { $and: [{ property: "value" }, { property: "value" }] };
+// $not
+const not = { $not: "value" };
+// $or
+const or = { $or: [{ property: "value" }, { property: "value" }] };
+// $nor
+const nor = { $nor: [{ property: "value" }, { property: "value" }] };
