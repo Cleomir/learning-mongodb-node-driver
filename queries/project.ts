b11bb8161cded9f4f326c6443381f6a64fe38d98
Cleomir <cleomir.mg@hotmail.com>
2020-10-15 10:22:32 +0300

    Add project

---
+++ queries/project.ts
@@ -0,0 +1,28 @@
+import { MongoClient, Db, Collection, Cursor } from "mongodb";
+
+const uri =
+  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+(async function run() {
+  try {
+    await client.connect();
+
+    const database: Db = client.db("db_name");
+    const collection: Collection<any> = database.collection("collections_name");
+    const result: Cursor<any> = collection
+      .find({
+        name: "some name",
+        date: {
+          $gte: new Date(new Date().setHours(0, 0, 0)),
+          $lt: new Date(new Date().setHours(23, 59, 59)),
+        },
+      })
+      .project({ name: 1, date: 0 });
+
+    await result.forEach((record) => console.log(record));
+    await client.close();
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+})();
