06d972f3c99b47996b5a5e0ebf6edc3bea15d976
Cleomir <cleomir.mg@hotmail.com>
2020-10-16 10:06:47 +0300

    Add findOneAndReplace

---
+++ compound-operations/findOneAndReplace.ts
@@ -0,0 +1,33 @@
+import {
+  MongoClient,
+  Db,
+  Collection,
+  FindAndModifyWriteOpResultObject,
+} from "mongodb";
+
+const uri =
+  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+(async function run() {
+  try {
+    await client.connect();
+
+    const database: Db = client.db("db_name");
+    const collection: Collection<any> = database.collection("collections_name");
+    const filter = { property: "value" };
+    const updateDocument = {
+      title: "test title",
+      date: new Date(),
+    };
+    const result: FindAndModifyWriteOpResultObject<any> = await collection.findOneAndReplace(
+      filter,
+      updateDocument
+    );
+
+    console.log(result.ok);
+    await client.close();
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+})();
