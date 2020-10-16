87c7b6c7586341edebd91ae63192da5493feb8d8
Cleomir <cleomir.mg@hotmail.com>
2020-10-16 10:06:35 +0300

    Add findOneAndDelete

---
+++ compound-operations/findOneAndDelete.ts
@@ -0,0 +1,28 @@
+import {
+  MongoClient,
+  Db,
+  Collection,
+  FindAndModifyWriteOpResultObject,
+} from "mongodb";
+
+const uri =
+  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+(async function run() {
+  try {
+    await client.connect();
+
+    const database: Db = client.db("db_name");
+    const collection: Collection<any> = database.collection("collections_name");
+    const filter = { property: "value" };
+    const result: FindAndModifyWriteOpResultObject<any> = await collection.findOneAndDelete(
+      filter
+    );
+
+    console.log(result.ok);
+    await client.close();
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+})();
